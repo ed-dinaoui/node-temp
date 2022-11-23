b81f51f781fe92c2584696988c6db81e6a4e43d6
ed-d <80011820+ed-dinaoui@users.noreply.github.com>
2022-11-23 05:51:56 -0500

    Update csb.nix

--- csb.nix
+++ csb.nix
@@ -4,6 +4,9 @@ stdenv.mkDerivation {
     name = "csb";
     buildInputs = [
         ffmpeg
+        youtube-dl-exec
+        express
+        cors
     ];
     shellHook = ''
         export PATH="$PWD/node_modules/.bin/:$PATH"
